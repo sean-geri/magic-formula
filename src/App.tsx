e7b69abe6dc0a2205be4e15c1334a42bce6385ba
Sean Geri <sean.geri.97@gmail.com>
2025-01-11 20:31:42 +0200

    sd

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React, {useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function printNumber(num: number) {
+    setTimeout(() => {
+        console.log(num)
+        if (num < 11) {
+            printNumber(num+1)
+        }
+    }, 1000);
+}
+
+function printNumbersWithDelay(current: number = 0) {
+    if (current > 10) return;
+
+    console.log(" ASd " + current);
+    setTimeout(() => {
+        printNumbersWithDelay(current + 1);
+    }, 1000);
+}
+
+
+
+
+function App() {
+
+    useEffect(() => {
+        printNumber(1)
+        printNumbersWithDelay();
+
+    }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+asd
+      </header>
+    </div>
+  );
+}
+
+export default App;
